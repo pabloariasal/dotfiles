fd35d9e4b611cbbb04a88eff6b054203e0181345
Pablo Arias <pabloariasal@gmail.com>
2019-10-07 23:51:52 +0200

    Added fzf shortcut for selecting git branch

--- zsh_custom/fzf.zsh
+++ zsh_custom/fzf.zsh
@@ -1,7 +1,27 @@
-
 #FZF settings
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_DEFAULT_OPTS='--height 40%'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fasd -Rld"
 [[ -r "/usr/share/fzf/key-bindings.zsh" ]] && source /usr/share/fzf/key-bindings.zsh
+
+# CTRL-O - Paste the selected git branch into the command line
+__fbranch() {
+  local cmd="git branch -a | grep -v HEAD"
+  setopt localoptions pipefail 2> /dev/null
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) "$@" | while read item; do
+    echo -n "${(q)item} " | awk '{print $1}' | sed "s/remotes\///"
+  done
+  local ret=$?
+  echo
+  return $ret
+}
+
+fzf-branch-widget() {
+  LBUFFER="${LBUFFER}$(__fbranch)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+zle     -N   fzf-branch-widget
+bindkey '^O' fzf-branch-widget
