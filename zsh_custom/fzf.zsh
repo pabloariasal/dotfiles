48d9acd7df0a44e2dd60a2e796a649be2525b463
Pablo Arias <pabloariasal@gmail.com>
2020-02-01 18:12:06 +0100

    Added directories to Ctrl-T fzf widget

--- zsh_custom/fzf.zsh
+++ zsh_custom/fzf.zsh
@@ -1,5 +1,5 @@
 #FZF settings
-export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_COMMAND='fd'
 export FZF_DEFAULT_OPTS='--height 40%'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # ALT-C cd into commond directory
