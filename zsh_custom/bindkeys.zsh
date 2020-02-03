77c6413f93ebe88e41b1505a4a36c7b4a246d015
Pablo Arias <pabloariasal@gmail.com>
2020-02-03 21:19:38 +0100

    Changed zsh-autosuggest binding

--- zsh_custom/bindkeys.zsh
+++ zsh_custom/bindkeys.zsh
@@ -1,3 +1,6 @@
 # Navigate the dir stack back and forth with alt+h and alt+l
 bindkey '^[h' insert-cycledleft
 bindkey '^[l' insert-cycledright
+
+# Accept autosuggestion with Ctrl+Space
+bindkey '^ ' autosuggest-accept
