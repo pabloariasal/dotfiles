c31cb7a24b8a29879aa56b406b1c8dda0c083011
Pablo Arias <pabloariasal@gmail.com>
2020-02-18 22:40:35 +0100

    Use hstr ranked history

--- zsh_custom/hstr.zsh
+++ zsh_custom/hstr.zsh
@@ -4,5 +4,5 @@ alias hh=hstr                    # hh to be alias for hstr
 setopt histignorespace           # skip cmds w/ leading space from history
 bindkey -s "\C-r" "\C-a hstr -- \C-j"     # bind hstr to Ctrl-r (for Vi mode check doc)
 bindkey -s "^[f" "\C-a hstr --favorites \C-j"
-export HSTR_CONFIG=hicolor,blacklist,raw-history-view,static-favorites
+export HSTR_CONFIG=hicolor,blacklist,static-favorites
 
