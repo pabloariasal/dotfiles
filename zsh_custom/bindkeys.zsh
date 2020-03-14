3a876695dd9aea063d96fe191e489d85de410433
Pablo Arias <pabloariasal@gmail.com>
2020-03-14 19:44:47 +0100

    Removed all unused configs

--- zsh_custom/bindkeys.zsh
+++
@@ -1,6 +0,0 @@
-# Navigate the dir stack back and forth with alt+h and alt+l
-bindkey '^[h' insert-cycledleft
-bindkey '^[l' insert-cycledright
-
-# Accept autosuggestion with Ctrl+Space
-bindkey '^ ' autosuggest-accept
--- zsh_custom/wal.zsh
+++
@@ -1,4 +0,0 @@
-# Import colorscheme from 'wal' asynchronously
-# &   # Run the process in the background.
-# ( ) # Hide shell job control messages.
-(cat ~/.cache/wal/sequences &)
