18a51559dde90b51141a5e8b20ca9aafcbd16777
Pablo Arias <pabloariasal@gmail.com>
2020-03-17 22:46:52 +0100

    Start polybar in every connected monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main &
-
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar main &
+  done
+else
+  polybar main &
+fi
 echo "Bars launched..."
