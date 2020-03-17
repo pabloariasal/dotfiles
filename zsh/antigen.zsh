22aa65eeab5213acdecad2403084a9525c2cfd81
Pablo Arias <pabloariasal@gmail.com>
2020-03-17 22:47:10 +0100

    Load zfm with antigen

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -5,6 +5,7 @@ antigen bundle ohmyzsh/ohmyzsh plugins/colored-man-pages
 antigen bundle ohmyzsh/ohmyzsh plugins/dircycle
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle pabloariasal/zfm
 
 # Load the theme.
 antigen theme romkatv/powerlevel10k
