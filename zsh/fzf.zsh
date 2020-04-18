9445ed4e0ccc74fca596ba1624c4d22c0d0e8987
Pablo Arias <pabloariasal@gmail.com>
2020-04-18 10:31:35 +0200

    change some small fzf options

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,7 +1,8 @@
 #FZF settings
 export FZF_DEFAULT_COMMAND='fd'
-export FZF_DEFAULT_OPTS='--height 40%'
+export FZF_DEFAULT_OPTS='--height 60%'
 export FZF_ALT_C_COMMAND="fd -td"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="--preview '(bat {} || tree -C {}) 2> /dev/null | head -200'"
 export FZF_CTRL_R_OPTS="--height 90% --sort --exact --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
 source ${HOME}/.fzf-key-bindings.zsh
