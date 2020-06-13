f0e8f6fb3077144052b192fb4e9664a5bb885995
Pablo Arias <pabloariasal@gmail.com>
2020-06-13 14:08:43 +0200

    fzf - always show history event in preview window

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,5 +4,5 @@ export FZF_DEFAULT_OPTS='--height 60%'
 export FZF_ALT_C_COMMAND="fd -td"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview '(bat {} || tree -C {}) 2> /dev/null | head -200'"
-export FZF_CTRL_R_OPTS="--height 90% --sort --exact --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+export FZF_CTRL_R_OPTS="--height 90% --sort --preview 'echo {}' --preview-window down:3:wrap --bind '?:toggle-preview'"
 source ${HOME}/.fzf-key-bindings.zsh
