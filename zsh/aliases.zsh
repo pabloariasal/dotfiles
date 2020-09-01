e4ae8c1b6bd48ece76d2d418c0b74c2d5c2ac09a
Pablo Arias <pabloariasal@gmail.com>
2020-09-02 01:10:00 +0200

    Better shortcut for vim sessions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,7 +18,7 @@ alias sx='devour sxiv'
 alias gka='devour gitk --all --branches'
 alias gk='devour gitk'
 
-function ns()
+function s()
 {
   if [[ $(pwd) == ${NOTES_DIR}/org ]]; then
     nvim -S ${HOME}/.org_session
