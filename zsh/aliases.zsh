6784956b6d974b2cca2e891bd753f6c46856b9a8
Pablo Arias <pabloariasal@gmail.com>
2020-09-20 19:25:21 +0200

    Small change in git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -26,3 +26,30 @@ function s()
     nvim -S Session.vim
   fi
 }
+
+# Git
+alias g='git'
+alias ga='git add'
+alias gaa='git add --all'
+alias gapa='git add --patch'
+alias gau='git add --update'
+alias gc='git commit -v'
+alias 'gca'='git commit -v --amend'
+alias 'gcan'='git commit -v --no-edit --amend'
+alias gcb='git checkout -b'
+alias gco='git checkout'
+alias gd='git diff'
+alias gdca='git diff --cached'
+alias gf='git fetch'
+alias gl='git log'
+alias glp='git log --stat -p'
+alias glo='git log --oneline --decorate'
+alias glg='git log --oneline --graph --decorate'
+alias glga='git log --oneline --graph --decorate --all'
+alias gm='git merge'
+alias gp='git push'
+alias 'gp!'='git push --force'
+alias grb='git rebase'
+alias gs='git status'
+alias gsta='git stash'
+alias 'gr!'='git reset --hard HEAD'
