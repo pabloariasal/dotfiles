b073c1c7e60c5199b1f81871e3c29b58edc8c1a2
Pablo Arias <pabloariasal@gmail.com>
2021-04-10 10:34:07 +0200

    add previe window for fzf git bindings

--- zsh/.zsh/git.zsh
+++ zsh/.zsh/git.zsh
@@ -3,7 +3,8 @@
 __fbranch() {
   local cmd="git branch -a | grep -v HEAD"
   setopt localoptions pipefail 2> /dev/null
-  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) "$@" | while read item; do
+  eval "$cmd" | $(__fzfcmd) --reverse --preview-window=down  --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' "$@" |
+    while read item; do
     echo -n "${(q)item} " | awk '{print $1}' | sed "s/remotes\///"
   done
   local ret=$?
@@ -21,11 +22,13 @@ zle     -N   fzf-git-branch-widget
 bindkey '^E' fzf-git-branch-widget
 
 #######################################################################
-# CTRL-F - Paste the selected git file into the command line
+# CTRL-S - Paste the selected git file into the command line
 __gitfile() {
-  local cmd="git status --short"
+  local cmd="git -c color.status=always status --short"
   setopt localoptions pipefail 2> /dev/null
-  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --multi $FZF_DEFAULT_OPTS" $(__fzfcmd) "$@" | while read item; do
+  eval "$cmd" | $(__fzfcmd) --multi --reverse  --ansi --nth 2..,.. --preview-window=down --preview  '(git diff --color=always -- {-1} | sed 1,4d; cat {-1})' |
+  cut -c4- | sed 's/.* -> //'  "$@" |
+  while read item; do
     echo -n "${(q)item} " | awk '{print $NF}' | tr '\r\n' ' '
   done
   local ret=$?
