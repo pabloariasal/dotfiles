8982e7e97a4ab855d9c0abcea399250a6036e016
Pablo Arias <pabloariasal@gmail.com>
2021-05-06 22:17:36 +0200

    disable previews in fzf

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -4,7 +4,6 @@ export FZF_DEFAULT_COMMAND="fd -tf ${COMMON_FD_OPTS}"
 export FZF_DEFAULT_OPTS='--cycle --height 60%'
 export FZF_ALT_C_COMMAND="fd -td ${COMMON_FD_OPTS}"
 export FZF_CTRL_T_COMMAND="fd ${COMMON_FD_OPTS}"
-export FZF_CTRL_T_OPTS="--preview '(bat {} || tree -C {}) 2> /dev/null | head -200'"
 export FZF_CTRL_R_OPTS="--no-height --no-reverse --exact --preview 'echo {}' --preview-window down:3:wrap --bind '?:toggle-preview'"
 source ${HOME}/.fzf-key-bindings.zsh
 bindkey '^n' fzf-file-widget
--- zsh/.zsh/git.zsh
+++ zsh/.zsh/git.zsh
@@ -3,7 +3,7 @@
 __fbranch() {
   local cmd="git branch -a | grep -v HEAD"
   setopt localoptions pipefail 2> /dev/null
-  eval "$cmd" | $(__fzfcmd) --reverse --preview-window=down  --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' "$@" |
+  eval "$cmd" | $(__fzfcmd) --reverse "$@" |
     while read item; do
     echo -n "${(q)item} " | awk '{print $1}' | sed "s/remotes\///"
   done
@@ -26,7 +26,7 @@ bindkey '^E' fzf-git-branch-widget
 __gitfile() {
   local cmd="git -c color.status=always status --short"
   setopt localoptions pipefail 2> /dev/null
-  eval "$cmd" | $(__fzfcmd) --multi --reverse  --ansi --nth 2..,.. --preview-window=down --preview  '(git diff --color=always -- {-1} | sed 1,4d; cat {-1})' |
+  eval "$cmd" | $(__fzfcmd) --multi --reverse  --ansi --nth 2..,.. |
   cut -c4- | sed 's/.* -> //'  "$@" |
   while read item; do
     echo -n "${(q)item} " | awk '{print $NF}' | tr '\r\n' ' '
