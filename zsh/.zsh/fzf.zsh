33b2439150490fb871846549be9e0c3b2d195033
Pablo Arias <pabloariasal@gmail.com>
2021-05-22 03:52:09 +0200

    fzf ctr-t show only directories

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -11,8 +11,9 @@ export FZF_ALT_C_OPTS="--bind='ctrl-space:reload(fd -td --hidden -E ".git")' --b
 # fuzzy-find files and directories and enter them in the current command line buffer
 # <c-space> shows hidden elements
 # <c-r> hides hidden elements
+# <c-d> shows only directories (including hidden)
 export FZF_CTRL_T_COMMAND="fd"
-export FZF_CTRL_T_OPTS="--bind='ctrl-space:reload(fd --hidden -E ".git")' --bind='ctrl-r:reload(fd)'"
+export FZF_CTRL_T_OPTS="--bind='ctrl-space:reload(fd --hidden -E ".git")' --bind='ctrl-r:reload(fd)' --bind='ctrl-d:reload(fd -td --hidden -E ".git")'"
 export FZF_CTRL_R_OPTS="--no-height --no-reverse --exact --preview 'echo {}' --preview-window down:3:wrap --bind '?:toggle-preview'"
 
 source ${HOME}/.fzf-key-bindings.zsh
