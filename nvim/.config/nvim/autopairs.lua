fc1f89bc271bb90286469033db26512cdadd88c3
Pablo Arias <pabloariasal@gmail.com>
2022-01-18 02:53:53 +0100

    nvim autopairs add space to pattern

--- nvim/.config/nvim/autopairs.lua
+++ nvim/.config/nvim/autopairs.lua
@@ -3,6 +3,16 @@ if not status_ok then
   return
 end
 
-npairs.setup{
- fast_wrap = {}
-}
+npairs.setup({
+    fast_wrap = {
+      map = '<M-e>',
+      chars = { '{', '[', '(', '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,%s] ]], '%s+', ''),
+      offset = 0, -- Offset from pattern match
+      end_key = '$',
+      keys = 'qwertyuiopzxcvbnmasdfghjkl',
+      check_comma = true,
+      highlight = 'Search',
+      highlight_grey='Comment'
+    },
+})
