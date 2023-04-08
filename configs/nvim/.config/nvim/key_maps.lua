d5986b7261015e020cef20545e29f33fa80557b0
Pablo Arias <pabloariasal@gmail.com>
2023-04-08 13:12:50 +0200

    remove yank highlight

--- configs/nvim/.config/nvim/key_maps.lua
+++ configs/nvim/.config/nvim/key_maps.lua
@@ -6,17 +6,6 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
 vim.keymap.set('n', "\\", ',', { noremap=true })
 vim.keymap.set({'n', 'v'}, ',', ':', { noremap=true })
 vim.keymap.set('i', 'jj', '<ESC>', { noremap=true })
