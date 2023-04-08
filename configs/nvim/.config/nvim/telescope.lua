621eb7df464b0cd413a52620b011fde580f39e81
Pablo Arias <pabloariasal@gmail.com>
2023-04-08 16:45:42 +0200

    add mapping for finding in current buffer

--- configs/nvim/.config/nvim/telescope.lua
+++ configs/nvim/.config/nvim/telescope.lua
@@ -63,13 +63,7 @@ vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = "Open Files"})
 vim.keymap.set('n', '<c-n>', builtin.buffers, { desc = "Navigate buffers" })
 vim.keymap.set('n', '<leader>gl', builtin.live_grep, { desc = "[G]rep [L]ive" })
 vim.keymap.set('n', '<leader>o', builtin.oldfiles, { desc = "Open [O]ld files" })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>j', builtin.current_buffer_fuzzy_find, { desc = "Open [O]ld files" })
 require('telescope').load_extension('neoclip')
 vim.keymap.set('n', '<leader>v', '<cmd>Telescope neoclip initial_mode=normal<cr>', { desc = 'open neoclip clipboard' })
 -- Lsp
