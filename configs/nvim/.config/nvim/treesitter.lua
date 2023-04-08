e25b33dc7e4f8bfdc11eb9f95ee919a8b45f8bde
Pablo Arias <pabloariasal@gmail.com>
2023-04-08 18:25:51 +0200

    add markdown treesitter

--- configs/nvim/.config/nvim/treesitter.lua
+++ configs/nvim/.config/nvim/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.install'.compilers = { "gcc" }
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"c","cpp","rust","bash","json","lua","toml","yaml","python", "haskell"},
+  ensure_installed = {"c","cpp","rust","bash","json","lua","toml","yaml","python","haskell", "markdown"},
   highlight = {
     enable = true,              -- false will disable the whole extension
     disable = {}
