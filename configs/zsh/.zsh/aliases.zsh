5a20828a5cfbcacaa063b7c384ed5e7568267deb
Pablo Arias <pabloariasal@gmail.com>
2023-04-10 08:48:52 +0200

    move git aliases to git config file

--- configs/zsh/.zsh/aliases.zsh
+++ configs/zsh/.zsh/aliases.zsh
@@ -2,7 +2,6 @@
 alias n='nvim'
 alias v='vifm'
 alias b='bat --plain'
-alias t='tmux'
 
 # convenience
 alias sudo='sudo '
@@ -56,48 +55,4 @@ function toggle_ctags()
   else
     echo "fd -tf --strip-cwd-prefix" > $file && echo "ctags enabled"
   fi
-}
-
-# Git
-alias gr='git restore'
-alias grs='git restore --staged'
-alias gs='git status'
-alias ga='git add'
-alias gaa='git add --all'
-alias gapa='git add --patch'
-alias gau='git add --update'
-alias gc='git commit -v'
-alias gcm='git commit -m'
-alias 'gca'='git commit -v --amend'
-alias 'gcan'='git commit -v --no-edit --amend'
-alias gcb='git checkout -b'
-alias gco='git checkout'
-alias gd='git diff'
-alias gdca='git diff --cached'
-alias gf='git fetch --prune'
-alias gl='git log'
-alias glp='git log --stat -p'
-alias gls='git log --stat'
-alias glg='git log --oneline --graph --decorate'
-alias glgd='git log --oneline --graph --decorate HEAD origin/dev'
-alias glgm='git log --oneline --graph --decorate HEAD origin/master'
-alias glga='git log --oneline --graph --decorate --all'
-alias glo='git log --oneline'
-alias gm='git merge'
-alias gp='git push'
-alias gpu='git push --set-upstream origin HEAD'
-alias gpf='git push --force'
-alias grb='git rebase'
-alias gsta='git stash'
-alias grh='git reset HEAD'
-alias grha='git reset --hard'
-alias grhh='git reset --hard HEAD'
-alias gb='git branch -vv'
-alias gba='git branch --all -vv'
-alias gcl='git clean -f -d'
-alias gw='git worktree'
-
-function gclb() {
-  git remote update --prune &&
-  git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
 }
--- configs/zsh/.zsh/git.zsh
+++ configs/zsh/.zsh/git.zsh
@@ -1,4 +1,6 @@
 #######################################################################
+# Fzf functions
+
 # CTRL-E - Paste the selected git branch into the command line
 __fbranch() {
   local cmd="git branch -a | grep -v HEAD"
@@ -21,7 +23,6 @@ fzf-git-branch-widget() {
 zle     -N   fzf-git-branch-widget
 bindkey '^E' fzf-git-branch-widget
 
-#######################################################################
 # CTRL-S - Paste the selected git file into the command line
 __gitfile() {
   local cmd="git -c color.status=always status --short"
@@ -44,3 +45,74 @@ fzf-git-file-widget() {
 }
 zle     -N   fzf-git-file-widget
 bindkey '^s' fzf-git-file-widget
+
+# CTRL-W - Jump to worktree
+# __gitfile() {
+#   local cmd="git -c color.status=always status --short"
+#   setopt localoptions pipefail 2> /dev/null
+#   eval "$cmd" | $(__fzfcmd) --multi --reverse  --ansi --nth 2..,.. |
+#   cut -c4- | sed 's/.* -> //'  "$@" |
+#   while read item; do
+#     echo -n "${(q)item} " | awk '{print $NF}' | tr '\r\n' ' '
+#   done
+#   local ret=$?
+#   echo
+#   return $ret
+# }
+
+# fzf-git-file-widget() {
+#   LBUFFER="${LBUFFER}$(__gitfile)"
+#   local ret=$?
+#   zle reset-prompt
+#   return $ret
+# }
+# zle     -N   fzf-git-file-widget
+# bindkey '^s' fzf-git-file-widget
+
+#######################################################################
+# Aliases
+alias gr='git restore'
+alias grs='git restore --staged'
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add --all'
+alias gapa='git add --patch'
+alias gau='git add --update'
+alias gc='git commit -v'
+alias gcm='git commit -m'
+alias 'gca'='git commit -v --amend'
+alias 'gcan'='git commit -v --no-edit --amend'
+alias gcb='git checkout -b'
+alias gco='git checkout'
+alias gd='git diff'
+alias gdca='git diff --cached'
+alias gf='git fetch --prune'
+alias gl='git log'
+alias glp='git log --stat -p'
+alias gls='git log --stat'
+alias glg='git log --oneline --graph --decorate'
+alias glgd='git log --oneline --graph --decorate HEAD origin/dev'
+alias glgm='git log --oneline --graph --decorate HEAD origin/master'
+alias glga='git log --oneline --graph --decorate --all'
+alias glo='git log --oneline'
+alias gm='git merge'
+alias gp='git push'
+alias gpu='git push --set-upstream origin HEAD'
+alias gpf='git push --force'
+alias grb='git rebase'
+alias gsta='git stash'
+alias grh='git reset HEAD'
+alias grha='git reset --hard'
+alias grhh='git reset --hard HEAD'
+alias gb='git branch -vv'
+alias gba='git branch --all -vv'
+alias gcl='git clean -f -d'
+alias gw='git worktree'
+
+#######################################################################
+# Functions
+
+function gclb() {
+  git remote update --prune &&
+  git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
+}
