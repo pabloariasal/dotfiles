85d0dcc95894b5b5af4a674df4e1f8ec8b2d0733
Pablo Arias <pabloariasal@gmail.com>
2023-05-10 22:33:49 +0200

    add comments to vim keymaps

--- configs/nvim/.config/nvim/key_maps.lua
+++ configs/nvim/.config/nvim/key_maps.lua
@@ -2,8 +2,11 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- reverse motion with backslash
 vim.keymap.set('n', "\\", ',', { noremap=true })
+-- open command line prompt with ,
 vim.keymap.set({'n', 'v'}, ',', ':', { noremap=true })
+-- exit insert mode with jj (best mapping ever)
 vim.keymap.set('i', 'jj', '<ESC>', { noremap=true })
 -- indent visual block
 vim.keymap.set('v', '<', '<gv', { desc = 'Indent visual block right' })
@@ -17,27 +20,28 @@ vim.keymap.set('n', '<leader>e', '<cmd>EditVifm<cr>', { desc = 'open file explor
 -- grepping
 vim.keymap.set('n', '<leader>gn', ':<c-u>grep! ', { desc = 'grep for word' })
 vim.keymap.set('n', '<leader>gc', ':<c-u>grep! <C-r>=expand("<cword>")<cr>', { desc = 'grep for word under cursor' })
--- tags
+-- jump to tag
 vim.keymap.set('n', '<leader>t', ':<c-u>tjump ', { desc = 'start tjump prompt' })
 -- -- quitting and saving
 vim.keymap.set('n', '<leader>w', '<cmd>wa<cr>', { desc = 'save all' })
 vim.keymap.set('n', '<leader>q', '<cmd>xa<cr>', { desc = 'save and quit' })
 vim.keymap.set('n', '<c-q>', '<cmd>xa!<cr>', { desc = 'force quit all' })
--- splits
+-- resize splits
 vim.keymap.set('n', '<c-left>', '<cmd>vertical resize +3<cr>', { desc = 'resize split to the left' })
 vim.keymap.set('n', '<c-right>', '<cmd>vertical resize -3<cr>', { desc = 'resize split to the right' })
 vim.keymap.set('n', '<c-up>', '<cmd>resize +3<cr>', { desc = 'resize split to the top' })
 vim.keymap.set('n', '<c-down>', '<cmd>resize -3<cr>', { desc = 'resize split to the bottom' })
+-- move to split
 vim.keymap.set('n', '<c-j>', '<c-w>j', { desc = 'move to split left' })
 vim.keymap.set('n', '<c-k>', '<c-w>k', { desc = 'move to split right' })
 vim.keymap.set('n', '<c-h>', '<c-w>h', { desc = 'move to split top' })
 vim.keymap.set('n', '<c-l>', '<c-w>l', { desc = 'move to split bottom' })
--- quickfix and location lists
+-- toggle quickfix and location lists
 vim.keymap.set('n', 'gq', '<Plug>(qf_qf_toggle)', { desc = 'toggle quickfix list' })
 vim.keymap.set('n', 'gl', '<Plug>(qf_loc_toggle)', { desc = 'toggle location list' })
--- search word
-vim.keymap.set({'n', 'v'}, 'z*', '*N', { desc = 'search for word under cursor' })
-vim.keymap.set({'n', 'v'}, 'z#', '#N', { desc = 'search backward for word under cursor' })
+-- search word under cursor without jumping
+vim.keymap.set({'n', 'v'}, 'z*', '*N', { desc = 'search for word under cursor no jump' })
+vim.keymap.set({'n', 'v'}, 'z#', '#N', { desc = 'search backward for word under cursor no jump' })
 -- reload config
 vim.keymap.set('n', '<F1>', '<cmd>source ~/.config/nvim/init.vim<cr>', { desc = 'reload config' })
 -- system clipboard
