7c3c84a3464274bbc990335d561901cde04a5d97
Pablo Arias <pabloariasal@gmail.com>
2023-05-23 22:02:44 +0200

    refactor lsp lua

--- configs/nvim/.config/nvim/lsp.lua
+++ configs/nvim/.config/nvim/lsp.lua
@@ -3,33 +3,35 @@ vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, { noremap=true, silent=true,
 vim.keymap.set('n', ']g', vim.diagnostic.goto_next, { noremap=true, silent=true, desc="Go to [n]ext [d]iagnostic" })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { noremap=true, silent=true, desc="Open [d]iagnostics" })
 
-local lspconfig = require'lspconfig'
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = 'LSP: ' .. desc
+      end
 
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+      vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = desc, silent=true, noremap=true })
     end
+    nmap('gD', vim.lsp.buf.declaration, 'Open [D]eclaration')
+    nmap('gd', vim.lsp.buf.definition, 'Open [D]efinition')
+    nmap('K', vim.lsp.buf.hover, 'Hover')
+    nmap('gI', vim.lsp.buf.implementation, 'Open [I]mplementation')
+    nmap('gy', vim.lsp.buf.type_definition, "Open Type Definition")
+    nmap('<leader>sh', vim.lsp.buf.signature_help, '[S]ignature [H]elp')
+    nmap('<F2>', vim.lsp.buf.rename, "Rename symbol")
+    -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    nmap('<leader>r', vim.lsp.buf.references, "Open [R]eferences")
+    nmap('<leader>f', vim.lsp.buf.format, "[F]ormat")
+  end,
+})
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc, silent=true, noremap=true })
-  end
-
-  -- nmap('gD', vim.lsp.buf.declaration, 'Open [D]eclaration')
-  nmap('gd', vim.lsp.buf.definition, 'Open [D]efinition')
-  nmap('gI', vim.lsp.buf.implementation, 'Open [I]mplementation')
-  nmap('gy', vim.lsp.buf.type_definition, "Open Type Definition")
-  nmap('K', vim.lsp.buf.hover, 'Hover')
-  nmap('<leader>sh', vim.lsp.buf.signature_help, '[S]ignature [H]elp')
-  nmap('<F2>', vim.lsp.buf.rename, "Rename symbol")
-  -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  nmap('<leader>r', vim.lsp.buf.references, "Open [R]eferences")
-  nmap('<leader>f', vim.lsp.buf.format, "[F]ormat")
-end
+local lspconfig = require'lspconfig'
 
 -- Haskell - hls
 lspconfig.hls.setup {
@@ -38,42 +40,18 @@ lspconfig.hls.setup {
         formattingProvider = "ormolu";
       }
     },
-  on_attach = on_attach,
-  -- capabilities = capabilities
 }
-
--- C++ - ccls
--- lspconfig.ccls.setup {
---   init_options = {
---     cache = {
---       directory = ".ccls-cache"
---     }
---   },
---   -- capabilities = capabilities
---   on_attach = on_attach
--- }
 
 -- C++ clangd
-require'lspconfig'.clangd.setup{
-  on_attach = on_attach,
-  -- capabilities = capabilities
-}
+lspconfig.clangd.setup{}
 
 -- Rust - rust-analyzer
-lspconfig.rust_analyzer.setup{
-  capabilities = capabilities,
-  on_attach = on_attach
-}
+lspconfig.rust_analyzer.setup{}
 
 -- Python - python-language-server
 lspconfig.pylsp.setup{
   cmd = {"pyls"},
-  -- capabilities = capabilities,
-  on_attach = on_attach
 }
 
 -- CMake - cmake-language-server
-lspconfig.cmake.setup{
-  on_attach = on_attach,
-  -- capabilities = capabilities
-}
+lspconfig.cmake.setup{}
