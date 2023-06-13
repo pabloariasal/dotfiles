5cde07c4fe44154f8d4ce40492d3c3bd739095a8
Pablo Arias <pabloariasal@gmail.com>
2023-06-13 21:46:22 +0200

    cleanup treesitter config

--- configs/nvim/.config/nvim/treesitter.lua
+++ configs/nvim/.config/nvim/treesitter.lua
@@ -3,7 +3,6 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = {"c","cpp","rust","bash","json","lua","toml","yaml","python","haskell", "markdown"},
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = {}
   },
   indent = {
     enable = true
