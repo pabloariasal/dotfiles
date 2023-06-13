0f3d22cb44dc3d76417343468090f9e2978bcee7
Pablo Arias <pabloariasal@gmail.com>
2023-06-13 22:02:48 +0200

    Revert "add markdown folding plugin"
    This reverts commit 63f046dce27c94966a130016b6bc59eb854115e8.

--- configs/nvim/.config/nvim/plugins.lua
+++ configs/nvim/.config/nvim/plugins.lua
@@ -58,7 +58,6 @@ return require('packer').startup(function()
       fast_wrap = {},
     } end
   }
-  use 'masukomi/vim-markdown-folding'
   -- colors
   use 'morhetz/gruvbox'
   use 'shaunsingh/solarized.nvim'
