4b8d4641cd8f3e1052badc1f87b47f5c97752f7c
Pablo Arias <pabloariasal@gmail.com>
2023-11-25 21:21:58 +0100

    cleanup user commands

--- configs/nvim/.config/nvim/user-commands.lua
+++ configs/nvim/.config/nvim/user-commands.lua
@@ -8,13 +8,8 @@ vim.api.nvim_create_user_command('BuffOnly', '%bd|e#', {})
 vim.api.nvim_create_user_command('SpellCheckGerman', 'set spell spelllang=de_de', {})
 -- turn on spell checking in englisch
 vim.api.nvim_create_user_command('SpellCheckEnglish', 'set spell spelllang=en_us', {})
--- open a tip like clippy
-function Vtip()
-  vim.api.nvim_echo({{vim.fn.system('curl -s ' .. "https://vtip.43z.one")}}, true, {})
-end
-vim.api.nvim_create_user_command('Vtip', Vtip, {})
 -- list snippets of current filetype
-vim.api.nvim_create_user_command('PrintSnippets', function(opts)
+vim.api.nvim_create_user_command('ListSnippets', function(opts)
   if #opts.fargs == 0 then
     vim.cmd('!list_snippets ' .. vim.bo.filetype)
   else
@@ -22,20 +17,22 @@ vim.api.nvim_create_user_command('PrintSnippets', function(opts)
   end
 end, { nargs='?'})
 -- Read template
---
 function getCurrentYear()
     local currentTime = os.time()
     return tonumber(os.date("%Y", currentTime))
 end
 
 vim.api.nvim_create_user_command('Template', function(opts)
-  vim.cmd('read ' .. opts.fargs[1])
-  local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
- if string.match(first_line, '^%s*$') then
-    vim.cmd('0d')
-  end
-  vim.cmd('%s/%YEAR%/' .. getCurrentYear() .. '/g')
-end,
+    -- read provided template file into buffer
+    vim.cmd('read ' .. opts.fargs[1])
+    -- remove the first line if empty
+    local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+     if string.match(first_line, '^%s*$') then
+      vim.cmd('0d')
+    end
+    -- replace all ocurrences of '%YEAR%' with the current year
+    vim.cmd('%s/%YEAR%/' .. getCurrentYear() .. '/g')
+  end,
   { nargs = 1,
     complete = function(ArgLead, CmdLine, CursorPos)
       return vim.split(vim.fn.glob('~/.config/nvim/templates/*'), '\n')
