2cceda2aba1f4972233f5757f4cc5ab88ea66236
Pablo Arias <pabloariasal@gmail.com>
2023-12-19 21:54:26 +0100

    remove unused function

--- configs/zsh/.zsh/functions.zsh
+++ configs/zsh/.zsh/functions.zsh
@@ -8,16 +8,6 @@ function tmp()
   fi
 }
 
-# link compile_comands.json
-function lcc() {
-  local compilation_database="$(fd --no-ignore compile_commands.json)"
-  if [[ -z "$compilation_database" ]]; then
-    echo "no compilation database found"
-    return 0
-  fi
-  ln -s -v "$compilation_database" .
-}
-
 function reveal_present()
 {
   ${CHROMIUM_EXEC} "$1" index.html
