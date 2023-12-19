6e4d4e040d0f01d902f60a77e04e1c9468e10923
Pablo Arias <pabloariasal@gmail.com>
2023-12-19 22:14:45 +0100

    add aliases for handling tarballs

--- configs/zsh/.zsh/functions.zsh
+++ configs/zsh/.zsh/functions.zsh
@@ -12,3 +12,51 @@ function reveal_present()
 {
   ${CHROMIUM_EXEC} "$1" index.html
 }
+
+##########################
+# Archives
+
+function extract() {
+  if [[ "$1" == *".tar.gz"* ]]; then
+    tar -xvzf "$1"
+  elif [[ "$1" == *".tar"* ]]; then
+    tar -xvf "$1"
+  elif [[ "$1" == *".zip"* ]]; then
+    unzip "$1"
+  else
+    echo "unknown format"
+    return 1
+  fi
+}
+
+function extract_to_dir() {
+   if [ ! "$#" -eq 2 ]; then
+        echo "Expected two arguments"
+        echo "usage: extract_to_dir <archive> <dir>"
+        return 1
+  fi
+  if [[ "$1" == *".tar.gz"* ]]; then
+    mkdir -p "$2"
+    tar -xvzf "$1" -C "$2"
+  elif [[ "$1" == *".tar"* ]]; then
+    tar -xvf "$1" -C "$2"
+  elif [[ "$1" == *".zip"* ]]; then
+    unzip -d "$2" "$1"
+  else
+    echo "unknown format"
+    return 1
+  fi
+}
+
+function list_archive() {
+  if [[ "$1" == *".tar.gz"* ]]; then
+    tar -tvzf "$1"
+  elif [[ "$1" == *".tar"* ]]; then
+    tar -tvf "$1"
+  elif [[ "$1" == *".zip"* ]]; then
+    unzip -l "$1"
+  else
+    echo "unknown format"
+    return 1
+  fi
+}
