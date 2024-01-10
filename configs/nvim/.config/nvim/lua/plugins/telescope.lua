50bf75c9a9c832d8f85992b32f0e0373ae2765c1
Pablo Arias <pabloariasal@gmail.com>
2024-01-10 15:08:19 +0100

    fix neoclip

--- configs/nvim/.config/nvim/lua/plugins/telescope.lua
+++ configs/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -72,13 +72,28 @@ return {
             { '<leader>o',        function() require('telescope.builtin').oldfiles() end,                      desc = "Open [O]ld files" },
             { '<leader><leader>', function() require('telescope.builtin').current_buffer_fuzzy_find() end,     desc = "Search in current buffer" },
             { '<leader>z',        function() require('telescope.builtin').command_history() end,               desc = 'open command window' },
-            { '<leader>v',        '<cmd>Telescope neoclip initial_mode=normal<cr>',                            desc = 'open neoclip clipboard' },
+            { '<leader>v',        function() require('telescope').extensions.neoclip.default() end,            desc = 'open neoclip clipboard' },
             { '<leader>i',        function() require('telescope.builtin').lsp_document_symbols() end,          desc = "Navigate [s]ymbols [d]ocument" },
             { '<leader>t',        function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end, desc = "Navigate [s]ymbols in workspace" },
         }
     },
     {
         "AckslD/nvim-neoclip.lua",
+        name = 'neoclip',
         dependencies = { 'nvim-telescope/telescope.nvim' },
+        opts = {
+            keys = {
+                telescope = {
+                    i = {
+                        select = '<c-l>',
+                        paste = '<cr>',
+                        paste_behind = '<c-h>',
+                    },
+                }
+            }
+        },
+        config = function(_, opts)
+            require('neoclip').setup(opts)
+        end
     }
 }
