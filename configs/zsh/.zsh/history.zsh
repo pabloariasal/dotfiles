d6de9a7303e1578a371af4127f9333407913495c
Pablo Arias <pabloariasal@gmail.com>
2024-01-23 22:40:35 +0100

    cleanup zsh history

--- configs/zsh/.zsh/history.zsh
+++ configs/zsh/.zsh/history.zsh
@@ -5,28 +5,37 @@ bindkey -M vicmd 'j' history-beginning-search-forward
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
 HISTSIZE=50000
-SAVEHIST=10000
+SAVEHIST=50000
+
+# Immediatelly append to history file
+setopt inc_append_history
 
 #share history across sessions
 setopt share_history
+
 # don't overwrite history file but append
 setopt append_history
+
 # remove duplicated lines first when history is full
 setopt hist_expire_dups_first
-# ignore duplicate entries in history listing (even of not adjacent)
+
+# ignore duplicate entries in history listing (even if not adjacent)
 setopt hist_ignore_all_dups
+
+# don't record an entry that was just recorded again
+setopt hist_ignore_dups
+
 # don't show adjacent duplicates when scrolling through history on the line editor
 setopt hist_find_no_dups
+
 # when expanding history (e.g. with !) don't execute directly but just populate input
 setopt hist_verify
-# save no duplicates in hist file
-#setopt hist_save_no_dups
-# extended history
+
+# save timestamp in history
 setopt extended_history
+
 # skip cmds with leading space from history
 setopt hist_ignore_space
-# Treated as a pattern at the time history files are written
-CMD_HISTORY_IGNORE="k|j|d|n|paru"
-GIT_HISTORY_IGNORE="g|gs|gc|gd|gs *|gdca|gapa|gl|glg|glg[dma]|gca|gcan|gp|gpf|gpu|gb|gba|grh|gm"
-LS_HISTORY_IGNORE="l|l[slat]"
-HISTORY_IGNORE="($CMD_HISTORY_IGNORE|$GIT_HISTORY_IGNORE|$LS_HISTORY_IGNORE)"
+
+# Dont write duplicate entries in the history file:
+setopt hist_save_no_dups
