ffe4d343472dabfc650240fa71d0de4c27ed3ca7
Pablo Arias <pabloariasal@gmail.com>
2024-05-24 22:27:55 +0200

    add dependencies of hardtime

--- configs/nvim/.config/nvim/lua/plugins/hardtime.lua
+++ configs/nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -1,5 +1,6 @@
 return {
     'm4xshen/hardtime.nvim',
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
         disable_mouse = false,
         restricted_keys = {
