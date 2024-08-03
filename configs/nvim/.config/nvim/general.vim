129856ba8494320e2b752ffa28c4f6da06af2d7d
Pablo Arias <pabloariasal@gmail.com>
2024-08-03 13:48:00 +0200

    cleanups nvim config

--- configs/nvim/.config/nvim/general.vim
+++
@@ -1,6 +0,0 @@
-"Load plugins per filetype
-filetype plugin on
-filetype plugin indent on
-
-"Enable syntax highlighting
-syntax on
--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -2,21 +2,16 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("config.lazy")
 
-vim.cmd('runtime general.vim')
+vim.cmd([[
+    "Load plugins per filetype
+    filetype plugin on
+    filetype plugin indent on
+
+    "Enable syntax highlighting
+    syntax on
+]])
 require("config.options")
 require("config.autocommands")
 require("config.key_maps")
