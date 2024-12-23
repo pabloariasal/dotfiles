c08fcdaee55c1c05147634f42d462507fb4ef677
Pablo Arias <pabloariasal@gmail.com>
2024-12-23 15:34:55 +0100

    fix buffonly

--- configs/nvim/.config/nvim/lua/config/user-commands.lua
+++ configs/nvim/.config/nvim/lua/config/user-commands.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_user_command('ClearWhitespace', '%s/\\s\\+$//', {})
 -- close all buffers
 vim.api.nvim_create_user_command('BuffCloseAll', 'bufdo bwipeout', {})
 -- close all buffers but the current one (similar to tabonly)
-vim.api.nvim_create_user_command('BuffOnly', '%bd|e#', {})
+vim.api.nvim_create_user_command('BuffOnly', '%bd|edit#|bd#', {})
 -- turn on spell checking in german
 vim.api.nvim_create_user_command('SpellCheckGerman', 'set spell spelllang=de_de', {})
 -- turn on spell checking in englisch
