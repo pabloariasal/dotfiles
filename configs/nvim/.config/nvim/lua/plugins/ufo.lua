cb89dc493094048b7e6189b4b8ef4408809a243d
Pablo Arias <pabloariasal@gmail.com>
2024-12-23 16:10:36 +0100

    fix ufo configuration

--- configs/nvim/.config/nvim/lua/plugins/ufo.lua
+++ configs/nvim/.config/nvim/lua/plugins/ufo.lua
@@ -2,7 +2,8 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = { "kevinhwang91/promise-async" },
     init = function()
-        vim.o.foldlevelstart = 1
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
         vim.o.foldnestmax = 4
         vim.o.foldenable = true
     end,
