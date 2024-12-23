3f52f56f726992f01aaa28f0f3b16ecce6a3a4e2
Pablo Arias <pabloariasal@gmail.com>
2024-12-23 15:16:52 +0100

    enable folds and add K binding

--- configs/nvim/.config/nvim/lua/plugins/ufo.lua
+++ configs/nvim/.config/nvim/lua/plugins/ufo.lua
@@ -4,10 +4,17 @@ return {
     init = function()
         vim.o.foldlevelstart = 99
         vim.o.foldnestmax = 4
+        vim.o.foldenable = true
     end,
     keys = {
         { "zR", function() require('ufo').openAllFolds() end,  desc = "Open all folds" },
         { "zM", function() require('ufo').closeAllFolds() end, desc = "Close all folds" },
+        { "K",  function()
+                    local winid = require('ufo').peekFoldedLinesUnderCursor()
+                    if not winid then
+                        vim.lsp.buf.hover()
+                    end
+                end, desc = "Peek/Hover fold/function" },
     },
     opts = {
         provider_selector = function(bufnr, filetype, buftype)
