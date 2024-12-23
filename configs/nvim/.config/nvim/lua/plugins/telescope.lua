33a97842a484b4d53744f3e5ae6df8f001ea302d
Pablo Arias <pabloariasal@gmail.com>
2024-12-23 14:25:10 +0100

    update telescope

--- configs/nvim/.config/nvim/lua/plugins/telescope.lua
+++ configs/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
+        tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         opts = function()
             local actions = require("telescope.actions")
