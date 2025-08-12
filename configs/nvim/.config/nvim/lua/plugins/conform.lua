6d82994eb751f02fb9910fd0cdacc75b18e522c9
Pablo Arias <pabloariasal@gmail.com>
2025-08-12 21:44:35 +0200

    use stylua as lua formatter

--- configs/nvim/.config/nvim/lua/plugins/conform.lua
+++ configs/nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,19 +1,26 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    formatters_by_ft = {
-      json = { 'jq' }
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            json = { "jq" },
+            lua = { "stylua" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
     },
-    default_format_opts = {
-      lsp_format = "fallback",
+    config = function(_, opts)
+        require("conform").setup(opts)
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true })
+            end,
+            desc = "[Format] document",
+        },
     },
-  },
-  config = function(_, opts)
-    require('conform').setup(opts)
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
-  keys = {
-    { "<leader>f", function() require('conform').format({ async = true }) end, desc = "[Format] document" },
-  },
-  lazy = false
+    lazy = false,
 }
