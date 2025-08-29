6c0bbb4e06429f72ba611338d03d8ec8c2d3c276
Pablo Arias <pabloariasal@gmail.com>
2025-08-29 21:51:36 +0200

    use yapf as python formatter

--- configs/nvim/.config/nvim/lua/plugins/conform.lua
+++ configs/nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
         formatters_by_ft = {
             json = { "jq" },
             lua = { "stylua" },
+            python = { "yapf" }
         },
         default_format_opts = {
             lsp_format = "fallback",
