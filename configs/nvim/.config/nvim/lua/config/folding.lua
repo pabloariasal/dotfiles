c58dbb6f003a6b8ac6d7f1549c22cef183e80e84
Pablo Arias <pabloariasal@gmail.com>
2025-08-29 21:56:13 +0200

    don't fold per default

--- configs/nvim/.config/nvim/lua/config/folding.lua
+++ configs/nvim/.config/nvim/lua/config/folding.lua
@@ -3,4 +3,4 @@ vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 -- vim.wo.foldtext = 'v:lua.vim.treesitter.foldtext()'
 vim.wo.foldnestmax = 4
 vim.o.foldlevelstart = 1
-vim.wo.foldenable = true
+vim.wo.foldenable = false
