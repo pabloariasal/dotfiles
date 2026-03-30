ed462f52a254e35c8abe4e8f5c636e1f9d61eaf5
Pablo Arias <pabloariasal@gmail.com>
2026-03-30 21:00:39 +0200

    add mini move

---
+++ configs/nvim/.config/nvim/lua/plugins/mini_move.lua
@@ -0,0 +1,19 @@
+return {
+    "nvim-mini/mini.move",
+    version = "*",
+    opts = {
+        mappings = {
+            -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+            -- left = "<M-h>",
+            -- right = "<M-l>",
+            up = "<M-k>",
+            down = "<M-j>",
+
+            -- Move current line in Normal mode
+            line_left = "",
+            line_right = "",
+            line_down = "",
+            line_up = "",
+        },
+    },
+}
