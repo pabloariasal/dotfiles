229d9157297415550a3be34a4363686d67b8cd31
Pablo Arias <pabloariasal@gmail.com>
2026-03-30 13:52:52 +0200

    remove uneeded config

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -3,15 +3,6 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require("config.lazy")
-
-vim.cmd([[
-    "Load plugins per filetype
-    filetype plugin on
-    filetype plugin indent on
-
-    "Enable syntax highlighting
-    syntax on
-]])
 require("config.options")
 require("config.autocommands")
 require("config.key_maps")
