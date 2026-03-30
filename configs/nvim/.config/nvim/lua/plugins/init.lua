32965031581c2dfc0af36d5309e44a10efd075cd
Pablo Arias <pabloariasal@gmail.com>
2026-03-30 21:24:59 +0200

    remove plugins/init.lua

--- configs/nvim/.config/nvim/lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        'romainl/vim-qf',
-        init = function()
-            vim.g.qf_max_height = 20
-        end
-    },
-}
---
+++ configs/nvim/.config/nvim/lua/plugins/vim-qf.lua
@@ -0,0 +1,6 @@
+return {
+    "romainl/vim-qf",
+    init = function()
+        vim.g.qf_max_height = 20
+    end,
+}
