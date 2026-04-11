a1133b1243ac5137a14c41f203412038ab698e45
Pablo Arias <pabloariasal@gmail.com>
2026-04-11 20:44:38 +0200

    add script to setup the inital shell

---
+++ initial-shell-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+stow --verbose=2 -d configs -t ~ env
+stow --verbose=2 -d configs -t ~ zsh
+
+if [ "$SHELL" != '/bin/zsh' ]; then
+    echo "ZSH is not the default shell! Add this line to the existing .profile:"
+    echo ""
+    echo "source ~/.zprofile"
+fi
