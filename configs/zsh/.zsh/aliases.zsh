c958bb04199075c8f1c64d6b095feda4dbbd2864
Pablo Arias <pabloariasal@gmail.com>
2026-04-11 21:01:35 +0200

    only create aliases when needed

--- configs/zsh/.zsh/aliases.zsh
+++ configs/zsh/.zsh/aliases.zsh
@@ -1,9 +1,22 @@
 # program shorcuts
-alias n='nvim'
-alias nd='nvim .'
-alias b='bat --plain'
-alias v='vifm -c only'
-alias vp='vifm -c view'
+
+if [ -x "$(command -v nvim)" ]; then
+  alias n='nvim'
+  alias nd='nvim .'
+fi
+
+if [ -x "$(command -v bat)" ]; then
+  alias b='bat --plain'
+fi
+
+if [ -x "$(command -v batcat)" ]; then
+  alias b='batcat --plain'
+fi
+
+if [ -x "$(command -v vifm)" ]; then
+  alias v='vifm -c only'
+  alias vp='vifm -c view'
+fi
 
 # convenience
 alias sudo='sudo '
@@ -15,24 +28,32 @@ alias h='history -r -E -35'
 
 # ls
 alias ls='ls --color=tty'
-alias l='eza'
-alias la='eza -a'
-alias ll='eza -l'
-alias lla='eza -la'
-alias lal='eza -la'
-alias lt='eza --tree'
+
+if [ -x "$(command -v eza)" ]; then
+  alias l='eza'
+  alias la='eza -a'
+  alias ll='eza -l'
+  alias lla='eza -la'
+  alias lal='eza -la'
+  alias lt='eza --tree'
+fi
+
+if [ -x "$(command -v exa)" ]; then
+  alias l='exa'
+  alias la='exa -a'
+  alias ll='exa -l'
+  alias lla='exa -la'
+  alias lal='exa -la'
+  alias lt='exa --tree'
+fi
 
 # add ssh keys to agent
 alias sag='ssh-add ~/.ssh/github_personal'
+alias ssh_config="${EDITOR} ~/.ssh/config"
 
-alias install_config='ensure_dots_dir && stow --verbose=2 -d configs -t ~ '
-alias ssh_config='nvim ~/.ssh/config'
-
-# Linux only aliases
-if [[ $OSTYPE == 'linux'* ]]; then
-
-  # devour
+if [ -x "$(command -v devour)" ]; then
   alias za='devour zathura'
   alias iv='devour eog'
 fi
 
+alias install_config='ensure_dots_dir && stow --verbose=2 -d configs -t ~ '
