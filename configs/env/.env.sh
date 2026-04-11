d1896fb9ec75d2006e0d058f1554297738d17ac6
Pablo Arias <pabloariasal@gmail.com>
2026-04-11 21:06:43 +0200

    simpifly check if running on arch

--- configs/env/.env.sh
+++ configs/env/.env.sh
@@ -5,6 +5,6 @@ export PATH="${HOME}/.local/bin:$PATH"
 
 # Tells SSH clients where to find the SSH agent — it contains the path to a Unix domain socket file that the agent is listening on.
 # on Arch, this must be set explicitly
-if [ -f /etc/os-release ] && grep -q '^ID=arch$' /etc/os-release; then
+if [ -f /etc/arch-release ]; then
     export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
--- configs/zsh/.zprofile
+++ configs/zsh/.zprofile
@@ -12,7 +12,7 @@ if [ "$(uname)" != "Darwin" ]; then
 fi
 
 # On Arch, start x server...
-if [ -f /etc/os-release ] && grep -q '^ID=arch$' /etc/os-release; then
+if [ -f /etc/arch-release ]; then
   if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
     exec startx
   fi
