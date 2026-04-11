96a9804ff5f462e3080c4653b459c16deb54416b
Pablo Arias <pabloariasal@gmail.com>
2026-04-11 20:02:04 +0200

    only set ssh_auth_sock on arch linux

--- configs/env/.env.sh
+++ configs/env/.env.sh
@@ -3,7 +3,12 @@ export EDITOR="nvim"
 export VISUAL="nvim"
 export PATH="${HOME}/.local/bin:$PATH"
 
-if [[ "$OSTYPE" != "darwin"* ]]; then
+# Tells SSH clients where to find the SSH agent — it contains the path to a Unix domain socket file that the agent is listening on.
+# on Arch, this must be set explicitly
+if [ -f /etc/os-release ] && grep -q '^ID=arch$' /etc/os-release; then
     export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+fi
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
     export CHROMIUM_EXEC=google-chrome-stable
 fi
